8df72cd022839d3212d1eb47e2583b9a7ffea80f
JJX <10248961+jjx-@users.noreply.github.com>
2024-09-25 20:09:47 +0000

    New API + Error Handling

--- usr/lib/sdrmapfeeder/sdrmapfeeder.sh
+++ usr/lib/sdrmapfeeder/sdrmapfeeder.sh
@@ -35,22 +35,22 @@ while true; do
 		},\
 		\"packages\":{\
 			\"c2isrepo\":\"$(cat /etc/apt/sources.list.d/*|grep -c 'https://repo.chaos-consulting.de')\",\
-			\"mlat-client-c2is\":\"$(dpkg -s mlat-client-c2is|grep 'Version:'|cut -d ' ' -f 2)\",\
-			\"stunnel4\":\"$(dpkg -s stunnel4|grep 'Version:'|cut -d ' ' -f 2)\",\
-			\"dump1090-mutability\":\"$(dpkg -s dump1090-mutability|grep 'Version:'|cut -d ' ' -f 2)\",\
-			\"dump1090-fa\":\"$(dpkg -s dump1090-fa|grep 'Version:'|cut -d ' ' -f 2)\",\
+			\"mlat-client-c2is\":\"$(dpkg -s mlat-client-c2is 2>&1|grep 'Version:'|cut -d ' ' -f 2)\",\
+			\"stunnel4\":\"$(dpkg -s stunnel4 2>&1|grep 'Version:'|cut -d ' ' -f 2)\",\
+			\"dump1090-mutability\":\"$(dpkg -s dump1090-mutability 2>&1|grep 'Version:'|cut -d ' ' -f 2)\",\
+			\"dump1090-fa\":\"$(dpkg -s dump1090-fa 2>&1|grep 'Version:'|cut -d ' ' -f 2)\",\
 			\"ais-catcher\":\"$(dpkg -s ais-catcher 2>&1 |grep 'Version:'|cut -d ' ' -f 2)\"\
 		},\
 		\"feeder\":{\
 			\"version\":\"$version\",\
 			\"interval\":\"$sysinfointerval\"
 		}\
-	}"| gzip -c |curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://adsb.chaos-consulting.de/sysinfoin/index.php
+	}"| gzip -c |curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://sys.feed.sdrmap.org/index.php
 	fi;
 
 	if [ adsb=='true' ]
 	then
-		gzip -c $adsbpath | curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://adsb.chaos-consulting.de/aircraftin/index.php
+		gzip -c $adsbpath | curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://adsb.feed.sdrmap.org/index.php
 	fi;
 
 	if [ radiosonde=='true' ] && [ $(($(date +"%s") - $radiosondelastrun)) -ge "$radiosondeinterval" ]
@@ -62,7 +62,7 @@ while true; do
 		fi;
 		for i in $(find $radiosondepath -mmin -0.1 -name "*sonde.log");
 			do
-			tail -n 1 $i | gzip | curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://adsb.chaos-consulting.de/radiosondein/index.php
+			tail -n 1 $i | gzip | curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://radiosonde.feed.sdrmap.org/index.php
 		done;
 	fi;
 
